78af826a28d4f901ef2e6810eedb77585fcdbabc
Demetrious Robinson <demetriusrobinson.dev@gmail.com>
2019-03-16 09:48:25 -0700

    merge

--- inkchallenger/settings.py
+++ inkchallenger/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'inkchallenger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inkchallenger',
+        'USER': 'inkchallenger',
+        'PASSWORD': 'inkchallenger',
+        'HOST': 'theinkchallenger.cdeq9svlh2ju.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
