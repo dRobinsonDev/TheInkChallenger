2e25c88843b9f66c93a19c2597748245012dc3c2
sun <soobaklee@gmail.com>
2019-03-18 10:38:52 -0700

    update css login

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
     path('contact/', views.Contact.as_view(), name="contact"),
-    path('tattoos/artists/', views.Artist.as_view(), name="artists"),
+    path('artists/', views.Artist.as_view(), name="artists"),
+    path('tattoos/', views.Tattoo.as_view(), name="tattoos"),
+    path('tattoos/appointments/', views.Appointment.as_view(), name="appointments"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.base import TemplateView
+
 
 # Create your views here.
 
@@ -15,3 +18,24 @@ class Contact(TemplateView):
 
 class Artist(TemplateView):
     template_name = 'artists.html'
+
+class Tattoo(TemplateView):
+    template_name = 'tattoos.html'
+
+class Appointment(TemplateView):
+    template_name = 'appointments.html'
+
+def signup(request):
+  error_message = ''
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request,user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid credentials - try again'
+
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
