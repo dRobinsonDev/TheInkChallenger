1631270ffe51d9ad26dda93a52f94871af4367d7
DanVieira7 <gisellademorais@Gisellas-MacBook-Pro.local>
2019-03-18 10:56:24 -0700

    updated models

--- main_app/models.py
+++ main_app/models.py
@@ -7,35 +7,7 @@ from django.db import models
 # If A belongs to B, A holds fk (id of B) and if 
 # upon deletion of B, A also needs to be deleted, than include cascade delete
 # Create your models here.
-class MyUser(AbstractUser):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
-    address = models.CharField(max_length=200)
-    birthday = models.DateField('Date')
     
-    # artists = models.OneToManyField(Artist) # does a user belong to an artist? if appt gets canceled, do we still want an association to artist?
-    # tattoos = models.OneToOneField(Tattoo) 
-    # tattoo will belong to an appointment or vice versa, do we need both?
-    # appointments = models.OneToOneField(Appointment)
-    
-class Artist(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    phone_number = models.IntegerField()
-    email = models.CharField(max_length=100)
-
-    # location = models.ManyToManyField(Location)
-    # appointment = models.ManyToManyField(Appointment)
-    # appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
-
-class Appointment(models.Model):
-    date = models.DateField('Date')
-    time = models.TimeField('Time')
-    completed = models.BooleanField()
-
-    # tattoos = models.OneToOneField(Tattoo)
-
 class Location(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField(max_length=240)
@@ -47,15 +19,39 @@ class Location(models.Model):
         ),
         size=2,
     )
-    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    email = models.CharField(max_length=100)
+
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
+
+class Appointment(models.Model):
+    date = models.DateField('Date')
+    time = models.TimeField('Time')
+    completed = models.BooleanField()
+    
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
 class Tattoo(models.Model):
     name = models.CharField(max_length=100)
     style = models.CharField(max_length=100)
     description = models.TextField(max_length=240)
-
+    available = models.BooleanField()
+    
     appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
 
-
-
-
+class MyUser(AbstractUser):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    address = models.CharField(max_length=200)
+    birthday = models.DateField('Date')
+    
+    tattoo = models.ForeignKey(Tattoo, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    
+    
