a5c71f47eed8572e7c9910d0dabb0fb8dc3994be
Demetrious Robinson <demetriusrobinson.dev@gmail.com>
2019-03-21 10:34:21 -0700

    merge

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     # path('about/', views.Ab   out.as_view(), name="about"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
-    path('artists/', views.Artist.as_view(), name="artists"),
+    path('artists/', views.ArtistList.as_view(), name="artists"),
     path('contact/', views.Contact.as_view(), name="contact"),
     path('events/', views.Create_Event, name='events'),
-    path('shops/', views.Shop.as_view(), name="shops"),
-    path('tattoos/', views.Tattoo.as_view(), name="tattoos"),
-    path('tattoos/appointment', views.Appointment.as_view(), name="appointments"),
+    path('shops/', views.ShopList.as_view(), name="shops"),
+    path('tattoos/', views.TattooList.as_view(), name="tattoos"),
+    path('tattoos/appointment', views.Create_Event, name="appointments"),
     path('tattoos/random', views.random_Tattoo, name="random_tattoo"),
 ]
 
